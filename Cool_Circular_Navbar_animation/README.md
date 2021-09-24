1641c0e9d6a11c2d4eef0ce31320699be2ecf489
Flippi91 <g.delphis@outlook.com>
2021-09-24 14:53:34 +0200

    Circular Navbar

---
+++ Cool_Circular_Navbar_animation/README.md
@@ -0,0 +1,1 @@
+# Things that might be cool for a website
---
+++ Cool_Circular_Navbar_animation/main.js
@@ -0,0 +1,6 @@
+'use strict'
+
+document.querySelector('.navbar-btn').addEventListener('click', () => {
+
+  document.querySelector('.navbar-wrapper').classList.toggle('change');
+});

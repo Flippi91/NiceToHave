828d6a92ea1beac7a893517993dd6e640574b657
Flippi91 <g.delphis@outlook.com>
2021-09-28 21:06:32 +0200

    added a new one

---
+++ 3D Navigatin Bar/main.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const hamburger_menu = document.querySelector(".hamburger-menu")
+const container = document.querySelector(".container")
+
+
+hamburger_menu.addEventListener('click', () => {
+    container.classList.toggle('active')
+})
